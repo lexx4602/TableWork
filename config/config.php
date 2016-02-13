3ebdf70ecc01e5297c2748e1fa5cce9c72fc6838
lexx <s_os@inbox.ru>
2016-02-13 17:21:07 +0300

    Config File

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lexx
+ * Date: 13.02.2016
+ * Time: 17:15
+ */
+
+// для подключения к бд
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'tablework');
