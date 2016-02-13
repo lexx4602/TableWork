c907ee6e0bfb2133800fc6c78285b2f456309aee
lexx <s_os@inbox.ru>
2016-02-13 22:12:35 +0300

    Общий коммит от 13.02

--- config/config.php
+++ config/config.php
@@ -7,6 +7,10 @@
  */
 
 // для подключения к бд
+define ('DS', DIRECTORY_SEPARATOR); // разделитель для путей к файлам
+$sitePath = realpath(dirname(__FILE__) . DS);
+define ('SITE_PATH', $sitePath); // путь к корневой папке сайта
+
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_HOST', 'localhost');
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lexx
+ * Date: 13.02.2016
+ * Time: 17:10
+ */
