3f5358c75caaaf9ffd45d73372f27361853ccfe3
root <root@localhost>
2016-02-25 15:28:58 +0300

    Controller_Main

---
+++ app/controllers/Controller_Main.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: a.borisov
+ * Date: 25.02.2016
+ * Time: 12:58
+ */
+class Controller_Main
+{
+function __construct()
+{
+    echo "Controller Main";
+}
+
+}
