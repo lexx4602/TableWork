b626700b117af9a202712ef9ac9730d7ebe1ebc7
root <root@localhost>
2016-03-01 18:11:35 +0300

    Day Commit

---
+++ app/controllers/Controller_Ftpserver.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: a.borisov
+ * Date: 25.02.2016
+ * Time: 15:44
+ */
+class Controller_Ftpserver
+{
+    function __construct()
+    {
+        echo "Controller_ftpserver";
+    }
+
+}
